9551e843ad4cc07c97badea756cdf9a2b35ca358
Ben Harrington <harringtonben@icloud.com>
2018-01-29 21:25:12 -0600

    finsihed exercise

--- LinqList/Program.cs
+++ LinqList/Program.cs
@@ -112,7 +112,93 @@ namespace LinqList
                 Console.WriteLine(number);
             }
 
+            // Build a collection of customers who are millionaires
+            List<Customer> customers = new List<Customer>()
+            {
+                new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+                new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+                new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+                new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+                new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+                new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+                new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+                new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+                new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+                new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+            };
+
+            var millionaires = customers.Where(customer => customer.Balance >= 1000000);
+
+            foreach (var millionare in millionaires)
+            {
+                Console.WriteLine(millionare.Name);
+            }
+
+            /* 
+                Given the same customer set, display how many millionaires per bank.
+                Ref: https://stackoverflow.com/questions/7325278/group-by-in-linq
+
+                Example Output:
+                WF 2
+                BOA 1
+                FTB 1
+                CITI 1
+            */
+            var millionaireBanks = customers.GroupBy(customer => customer.Bank);
+
+            foreach (var bank in millionaireBanks)
+            {
+                Console.WriteLine($"{bank.Key} {bank.Count()}");
+            }
+
+            /*
+                TASK:
+                As in the previous exercise, you're going to output the millionaires,
+                but you will also display the full name of the bank. You also need
+                to sort the millionaires' names, ascending by their LAST name.
+
+                Example output:
+                    Tina Fey at Citibank
+                    Joe Landy at Wells Fargo
+                    Sarah Ng at First Tennessee
+                    Les Paul at Wells Fargo
+                    Peg Vale at Bank of America
+            */
+
+
+            List<Bank> banks = new List<Bank>()
+            {
+                new Bank(){ Name="First Tennessee", Symbol="FTB"},
+                new Bank(){ Name="Wells Fargo", Symbol="WF"},
+                new Bank(){ Name="Bank of America", Symbol="BOA"},
+                new Bank(){ Name="Citibank", Symbol="CITI"}
+            };
+
+            var moreMillionaires = from customer in customers
+                                   join bank in banks on customer.Bank equals bank.Symbol
+                                   where (customer.Balance >= 1000000)
+                                   select new { CName = customer.Name, CBank = bank.Name};
+
+            foreach (var person in moreMillionaires)
+            {
+                Console.WriteLine($"{person.CName} is at {person.CBank}");
+            }
+
             Console.Read();
+
         }
+    }
+
+    public class Customer
+    {
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public string Bank { get; set; }
+    }
+
+    public class Bank
+    {
+        public string Symbol { get; set; }
+        public string Name { get; set; }
     }
 }

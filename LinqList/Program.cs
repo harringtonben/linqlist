e176632c5a1cda1d2f88d2813aef111323820db8
Ben Harrington <harringtonben@icloud.com>
2018-01-29 20:21:37 -0600

    finished 2 exercises

--- LinqList/Program.cs
+++ LinqList/Program.cs
@@ -10,6 +10,28 @@ namespace LinqList
     {
         static void Main(string[] args)
         {
+            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+
+            var lwords = fruits.Where(fruit => fruit.StartsWith("L"));
+
+            foreach (var letter in lwords)
+            {
+                Console.WriteLine(letter);
+            }
+
+            List<int> numbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            var fourSixMultiples = numbers.Where(number => number % 4 == 0 || number % 6 == 0);
+
+            foreach (var multiple in fourSixMultiples)
+            {
+                Console.WriteLine(multiple);
+            }
+
+            Console.Read();
         }
     }
 }

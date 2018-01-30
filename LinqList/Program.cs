ba70fa08cd222987f55f0570cff4cbf09f8079b0
Ben Harrington <harringtonben@icloud.com>
2018-01-29 20:40:35 -0600

    finished aggregate operations

--- LinqList/Program.cs
+++ LinqList/Program.cs
@@ -10,6 +10,7 @@ namespace LinqList
     {
         static void Main(string[] args)
         {
+            // Find the words in the collection that start with the letter 'L'
             List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
 
             var lwords = fruits.Where(fruit => fruit.StartsWith("L"));
@@ -19,6 +20,7 @@ namespace LinqList
                 Console.WriteLine(letter);
             }
 
+            // Which of the following numbers are multiples of 4 or 6
             List<int> numbers = new List<int>()
             {
                 15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
@@ -31,6 +33,7 @@ namespace LinqList
                 Console.WriteLine(multiple);
             }
 
+            // Order these student names alphabetically, in descending order (Z to A)
             List<string> names = new List<string>()
             {
                 "Heather", "James", "Xavier", "Michelle", "Brian", "Nina",
@@ -46,6 +49,49 @@ namespace LinqList
             {
                 Console.WriteLine(name);
             }
+
+            // Build a collection of these numbers sorted in ascending order
+            List<int> numbers2 = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            var numbersUp = numbers2.OrderBy(number => number);
+
+            foreach (var number in numbersUp)
+            {
+                Console.WriteLine(number);
+            }
+
+            // Output how many numbers are in this list
+            List<int> numbers3 = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            var howManyNumbers = numbers3.Count();
+
+            Console.WriteLine($"howManyNumbers has {howManyNumbers} numbers in it.");
+
+            // How much money have we made?
+            List<double> purchases = new List<double>()
+            {
+                2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
+            };
+
+            var totalAmount = purchases.Sum();
+
+            Console.WriteLine($"The total amount of purchases is {totalAmount}.");
+
+            // What is our most expensive product?
+            List<double> prices = new List<double>()
+            {
+                879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
+            };
+
+            var priceMax = prices.Max(price => price);
+
+            Console.WriteLine(priceMax);
 
             Console.Read();
         }
